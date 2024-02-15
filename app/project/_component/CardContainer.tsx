c6609d18c364c3ff35678f99119bcef513bcdfe6
lunarmoon7 <zentechie7@gmail.com>
2024-02-15 09:47:33 +0900

    style: 채팅박스 컴포넌트 추가
    - 레이아웃 테스트용으로 ChatBox 컴포넌트를 추가했습니다.
    - messages는 임시 메시지 배열입니다.

--- app/project/_component/CardContainer.tsx
+++ app/project/_component/CardContainer.tsx
@@ -10,6 +10,53 @@ import CardFront from "./CardFront";
 import CardBack from "./CardBack";
 import ChatCard from "./ChatCard";
 
+const messages = [
+  {
+    time: "오후 7:27",
+    content: "안녕하세요?",
+  },
+  {
+    time: "오후 7:29",
+    content: "아 넵 안녕하세요. 미리 와 계시나요?",
+  },
+  {
+    time: "오후 7:29",
+    content: "저는 앞입니다!",
+  },
+  {
+    time: "오후 7:30",
+    content: "어, 저도 카페에 들어와있는데",
+  },
+  {
+    time: "오후 7:30",
+    content: "무슨 색 옷 입고 계시나요?",
+  },
+  {
+    time: "오후 7:30",
+    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
+  },
+  {
+    time: "오후 7:30",
+    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
+  },
+  {
+    time: "오후 7:30",
+    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
+  },
+  {
+    time: "오후 7:30",
+    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
+  },
+  {
+    time: "오후 7:30",
+    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
+  },
+  {
+    time: "오후 7:30",
+    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
+  },
+];
+
 function CardContainer() {
   return (
     <Tabs defaultValue="card" className="h-[550px] w-[330px] sm:w-[450px]">
@@ -19,14 +66,14 @@ function CardContainer() {
       </TabsList>
       <TabsContent value="card" className="group h-full [perspective:1000px]">
         <div
-          className={`relative h-full transition-all duration-500 [transform-style:preserve-3d] `}
+          className={`relative h-full transition-all duration-500 [transform-style:preserve-3d]`}
         >
           <CardFront />
           <CardBack />
         </div>
       </TabsContent>
       <TabsContent value="chat" className="h-full">
-        <ChatCard />
+        <ChatCard messages={messages} />
       </TabsContent>
     </Tabs>
   );
--- app/project/_component/ChatCard.tsx
+++ app/project/_component/ChatCard.tsx
@@ -7,15 +7,49 @@ import {
 } from "@/app/_common/shadcn/ui/card";
 import { Input } from "@/app/_common/shadcn/ui/input";
 import { Button } from "@/app/_common/shadcn/ui/button";
+import ChatBox from "./ChatBox";
 
-export default function ChatCard() {
+interface ChatCardProps {
+  messages: {
+    time: string;
+    content: string;
+  }[];
+}
+
+export default function ChatCard(props: ChatCardProps) {
+  const { messages } = props;
+
   return (
     <Card className="overflow-hidden shadow-md">
       <CardHeader className="border-b bg-white py-4">
         <CardTitle className="text-base">📍 맥심플랜트 이태원점</CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col p-0 px-0">
-        <section className="flex max-h-96 flex-col gap-5 overflow-y-scroll px-5 py-2"></section>
+        <section className="flex max-h-96 flex-col gap-5 overflow-y-scroll px-5 py-2">
+          {messages.map(({ time, content }, idx) => {
+            if (idx % 2 === 0) {
+              return (
+                <ChatBox
+                  key={content}
+                  direction="flex-row"
+                  align="self-end"
+                  time={time}
+                  content={content}
+                />
+              );
+            } else {
+              return (
+                <ChatBox
+                  key={content}
+                  direction="flex-row-reverse"
+                  align="self-start"
+                  time={time}
+                  content={content}
+                />
+              );
+            }
+          })}
+        </section>
         <div className="flex h-24 grow items-center gap-2 border-t px-5">
           <Input type="text" placeholder="대화를 입력하세요." />
           <Button>전송</Button>
