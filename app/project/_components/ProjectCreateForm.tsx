a21c67d2c18c8904617ab120ffbb4775009267bf
lunarmoon7 <zentechie7@gmail.com>
2024-02-16 10:47:52 +0900

    feat: 상위 Form Schema 정의 & validation 추가
    - react-hook-form & zod를 사용하여 validation을 진행합니다.

---
+++ app/project/_components/ProjectCreateForm.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/app/_common/shadcn/ui/button";
+import { Form, FormMessage } from "@/app/_common/shadcn/ui/form";
+import { Separator } from "@/app/_common/shadcn/ui/separator";
+import { DialogClose } from "@radix-ui/react-dialog";
+import FormPlace from "./FormPlace";
+import FormTag from "./FormTag";
+import FormTime from "./FormTime";
+
+const formSchema = z
+  .object({
+    place: z.string().min(2, {
+      message: "장소를 설정하세요.",
+    }),
+    startHour: z.string(),
+    startMinute: z.string(),
+    endHour: z.string(),
+    endMinute: z.string(),
+    tags: z.undefined(),
+  })
+  .refine(
+    data => {
+      if (data.startHour === data.endHour) {
+        return data.startMinute < data.endMinute;
+      }
+      return data.startHour < data.endHour;
+    },
+    {
+      message: "종료 시각이 시작 시각보다 이릅니다.",
+      path: ["startMinute"],
+    },
+  );
+
+interface ProjectCreateFormProps {
+  onClose: (open: boolean) => void;
+}
+
+function ProjectCreateForm(props: ProjectCreateFormProps) {
+  const { onClose } = props;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      startHour: new Date().getHours().toString(),
+      startMinute: "0",
+      endHour: new Date().getHours().toString(),
+      endMinute: "0",
+    },
+  });
+
+  // TODO: 서버로 form 데이터 전송하는 로직 추가
+  const handleOnSubmit = (values: z.infer<typeof formSchema>) => {
+    onClose && onClose(false);
+  };
+
+  const handleOnPressEnter = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleOnSubmit)}
+        onKeyDown={handleOnPressEnter}
+        className="flex h-full w-full flex-col justify-between space-y-5"
+      >
+        <section>
+          <main className="flex flex-col gap-7">
+            <FormPlace form={form} />
+            <aside className="flex flex-col gap-5">
+              <div className="flex justify-between gap-3">
+                <FormTime
+                  form={form}
+                  label="🕡 시작"
+                  hourFieldName="startHour"
+                  minuteFieldName="startMinute"
+                />
+                <Separator
+                  orientation="vertical"
+                  color="black"
+                  className="h-20 self-end"
+                />
+                <FormTime
+                  form={form}
+                  label="🕡 종료"
+                  hourFieldName="endHour"
+                  minuteFieldName="endMinute"
+                />
+              </div>
+              {form.formState.errors.startMinute && (
+                <FormMessage>종료 시각이 시작 시각보다 이릅니다.</FormMessage>
+              )}
+            </aside>
+            <FormTag form={form} />
+          </main>
+        </section>
+        <footer className="flex justify-end gap-1">
+          <Button type="submit">생성</Button>
+          <DialogClose asChild>
+            <Button type="button">취소</Button>
+          </DialogClose>
+        </footer>
+      </form>
+    </Form>
+  );
+}
+
+export default ProjectCreateForm;
