170375588c53847040dcd3f69502e09b29a76ec3
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-02-16 10:58:31 +0900

    feat: 이전 클릭한 지역을 변수에 저장하여 애니메이션 클래스 삭제 로직 추가

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import MapComponent from "@/app/_common/components/MapComponent";
 import { MouseEvent } from "react";
 
+import MapComponent from "@/app/_common/components/MapComponent";
+
 function Map() {
+  let previousRegion: SVGElement | null = null;
   const handleRegionClick = (event: MouseEvent<HTMLDivElement>) => {
     const { pageX, pageY } = event;
     const { offsetWidth: width, offsetHeight: height } = document.body;
@@ -15,11 +17,18 @@ function Map() {
       if (tagName === "path") return;
       event.currentTarget.classList.remove("touch-none");
       event.currentTarget.style.transform = "";
+      if (previousRegion instanceof SVGElement) {
+        previousRegion.classList.remove("animate-map-bounce");
+        previousRegion = null;
+      }
     } else {
       if (tagName !== "path") return;
       event.currentTarget.classList.add("touch-none");
       event.currentTarget.style.transform = `scale(2.5) translate(${x}px, ${y}px)`;
+      if (!previousRegion && parentElement instanceof SVGElement) {
+        previousRegion = parentElement;
         parentElement.classList.add("animate-map-bounce");
+      }
     }
   };
 
