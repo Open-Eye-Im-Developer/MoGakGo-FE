40ebf5df3c1b0703d8907fc889ae65fe7796ca40
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-02-16 10:52:18 +0900

    feat: 지도 페이지 생성

---
+++ app/map/page.tsx
@@ -0,0 +1,13 @@
+import MapComponent from "@/app/_common/components/MapComponent";
+
+function Map() {
+  return (
+    <div
+      className="flex h-screen w-screen items-center justify-center p-4 transition-all duration-1000"
+    >
+      <MapComponent />
+    </div>
+  );
+}
+
+export default Map;
