6bd6305a746509a070eb3930e07f4d8017f14666
lunarmoon7 <zentechie7@gmail.com>
2024-02-17 18:31:36 +0900

    refactor: portal 위치 변경
    - Dialog가 위치하는 Portal의 위치를 변경했습니다.

--- app/project/_components/ProjectDialog.tsx
+++ app/project/_components/ProjectDialog.tsx
@@ -37,24 +37,25 @@ function ProjectDialog() {
           <IconPlus />
         </Button>
       </DialogTrigger>
-      <DialogPortal />
-      <DialogOverlay className="bg-white backdrop-blur-md" />
-      <DialogContent className="flex h-[550px] max-w-[325px] flex-col items-start gap-10 rounded-lg border-none bg-transparent p-0 [perspective:1000px]">
-        <div
-          onClick={handleFlip}
-          className={`relative h-full w-full transition-all duration-500 [transform-style:preserve-3d] ${activeCard ? "[transform:rotateY(180deg)]" : ""}`}
-        >
-          <Card className="absolute inset-0 left-0 top-0 flex h-full flex-col shadow-md [backface-visibility:hidden] ">
-            <CardHeader>
-              <CardTitle>프로젝트 카드 생성</CardTitle>
-            </CardHeader>
-            <CardContent className="grow">
-              <ProjectCreateForm onClose={setOpen} />
-            </CardContent>
-          </Card>
-          <CardBack />
-        </div>
-      </DialogContent>
+      <DialogPortal>
+        <DialogOverlay className="bg-white backdrop-blur-md" />
+        <DialogContent className="flex h-[550px] max-w-[325px] flex-col items-start gap-10 rounded-lg border-none bg-transparent p-0 [perspective:1000px]">
+          <div
+            onClick={handleFlip}
+            className={`relative h-full w-full transition-all duration-500 [transform-style:preserve-3d] ${activeCard ? "[transform:rotateY(180deg)]" : ""}`}
+          >
+            <Card className="absolute inset-0 left-0 top-0 flex h-full flex-col shadow-md [backface-visibility:hidden] ">
+              <CardHeader>
+                <CardTitle>프로젝트 카드 생성</CardTitle>
+              </CardHeader>
+              <CardContent className="grow">
+                <ProjectCreateForm onClose={setOpen} />
+              </CardContent>
+            </Card>
+            <CardBack onRotate={handleFlip} />
+          </div>
+        </DialogContent>
+      </DialogPortal>
     </Dialog>
   );
 }
