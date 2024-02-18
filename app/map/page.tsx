eae8f90a8d2fdeea8f1a0b5bf371ce7b313ab940
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-02-18 13:32:55 +0900

    feat: 법정구역코드에 해당하는 구역 확대하는 함수 추가

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -7,6 +7,20 @@ import MapComponent from "@/app/_common/components/MapComponent";
 import REGION_CODE from "@/app/_common/constants/regionCode";
 
 function Map() {
+  const zoomRegion = (regionCode: string) => {
+    const map = document.querySelector("#map-wrap") as HTMLDivElement;
+    const regionName = Object.keys(REGION_CODE).find(
+      region => REGION_CODE[region] === regionCode,
+    );
+    const region = document.querySelector(`#${regionName}`);
+
+    if (!region || !map) return;
+
+    const { bottom, top, right, left } = region.getBoundingClientRect();
+    const { offsetWidth: width, offsetHeight: height } = document.body;
+    map.style.transform = `scale(2.5) translate(${width / 2 - (right + left) / 2}px, ${height / 2 - (bottom + top) / 2}px)`;
+  };
+
   let previousRegion: SVGElement | null = null;
   const handleRegionClick = (event: MouseEvent<HTMLDivElement>) => {
     const { pageX, pageY } = event;
