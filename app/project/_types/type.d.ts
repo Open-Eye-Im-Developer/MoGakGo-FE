72653c0381fc210d87597eb7139fe98f6c835625
lunarmoon7 <zentechie7@gmail.com>
2024-02-18 13:58:16 +0900

    refactor: 공통으로 사용되는 타입 추상화
    - 검색 결과로 얻은 장소 아이템을 의미하는 PlaceItem 타입을 추상화 했습니다.

--- app/project/_types/type.d.ts
+++ app/project/_types/type.d.ts
@@ -7,6 +7,25 @@ interface Form {
   tags?: string[] | undefined;
 }
 
+interface PlaceItem {
+  place_name: string;
+  distance: string;
+  place_url: string;
+  category_name: string;
+  address_name: string;
+  road_address_name: string;
+  id: string;
+  phone: string;
+  category_group_code: string;
+  category_group_name: string;
+  x: string;
+  y: string;
+}
+
 interface IFormProps {
   form: UseFormReturn<Form>;
+}
+
+interface IPlaceResponseProps {
+  documents: PlaceItem[];
 }
