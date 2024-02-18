a21a42135e6760b95f82b0ed9096d18e24b0b83c
lunarmoon7 <zentechie7@gmail.com>
2024-02-18 18:32:50 +0900

    style: DialogContent의 height 수정

--- app/project/_components/ProjectDialog.tsx
+++ app/project/_components/ProjectDialog.tsx
@@ -37,7 +37,7 @@ function ProjectDialog() {
       </DialogTrigger>
       <DialogPortal>
         <DialogOverlay className="bg-white backdrop-blur-md" />
-        <DialogContent className="flex h-[550px] max-w-[325px] flex-col items-start gap-10 rounded-lg border-none bg-transparent p-0 [perspective:1000px]">
+        <DialogContent className="flex h-[600px] max-w-[325px] flex-col items-start gap-10 rounded-lg border-none bg-transparent p-0 [perspective:1000px]">
           <div
             className={`relative h-full w-full transition-all duration-500 [transform-style:preserve-3d] ${flipped ? "[transform:rotateY(180deg)]" : ""}`}
           >
