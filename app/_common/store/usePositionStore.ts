55a0179dd6dc9eb8d9b95b235f8d8b28d8dc1f2a
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-02-19 11:46:08 +0900

    feat: error callback 함수 설정

--- app/_common/store/usePositionStore.ts
+++ app/_common/store/usePositionStore.ts
@@ -20,7 +20,7 @@ const usePositionStore = create(
         navigator.geolocation.getCurrentPosition((position) => {
           const { longitude, latitude } = position.coords;
           set({ longitude, latitude });
-        });
+        }, errorCallback);
       },
       getPosition: () => {
         const { longitude, latitude } = get();
@@ -32,5 +32,22 @@ const usePositionStore = create(
     },
   ),
 );
+
+const errorCallback = (error: GeolocationPositionError) => {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      console.error("사용자가 Geolocation API의 사용 요청을 거부했습니다");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      console.error("가져온 위치 정보를 사용할 수 없습니다.");
+      break;
+    case error.TIMEOUT:
+      console.error("위치 정보를 가져오기 위한 요청이 허용 시간을 초과했습니다");
+      break;
+    default:
+      console.error("알 수 없는 오류가 발생했습니다.");
+      break;
+  }
+};
 
 export default usePositionStore;
