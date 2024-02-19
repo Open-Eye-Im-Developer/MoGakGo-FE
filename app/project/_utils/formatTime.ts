61bb5ceb9dcbc9c861415db220933fb83f987a16
lunarmoon7 <zentechie7@gmail.com>
2024-02-19 13:16:56 +0900

    refactor: 시간 포맷팅 변경
    - UTC 기준으로 변경했습니다.

--- app/project/_utils/formatTime.ts
+++ app/project/_utils/formatTime.ts
@@ -2,9 +2,10 @@ const formatTime = (sh: string, sm: string, eh: string, em: string) => {
   const startTime = new Date();
   const endTime = new Date();
 
-  startTime.setHours(Number(sh) + 9);
+  startTime.setUTCHours(Number(sh));
   startTime.setMinutes(Number(sm));
-  endTime.setHours(Number(eh) + 9);
+
+  endTime.setUTCHours(Number(eh));
   endTime.setMinutes(Number(em));
 
   const meetStartTime = startTime.toISOString().split(".")[0];
