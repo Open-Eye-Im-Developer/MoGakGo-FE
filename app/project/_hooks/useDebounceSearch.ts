2eec774d9f4ff577c1adfa1aeca1ec4d67d22574
lunarmoon7 <zentechie7@gmail.com>
2024-02-19 10:32:35 +0900

    feat: 선택한 장소의 위, 경도 저장 로직 추가
    - 검색 결과 목록으로 얻은 장소를 선택할 시, 해당 장소의 위, 경도를 저장합니다.

--- app/project/_hooks/useDebounceSearch.ts
+++ app/project/_hooks/useDebounceSearch.ts
@@ -15,6 +15,8 @@ function useDebounceSearch(form: IFormProps["form"]) {
     setOverlay(false);
     setPlaceInput(place.place_name);
     form.setValue("place", place.place_name);
+    form.setValue("lat", Number(place.y));
+    form.setValue("lng", Number(place.x));
   };
 
   useEffect(() => {
