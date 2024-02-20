3711e464080a5888c9e6387b5e2d3590b20b5236
bel1c10ud <mail@bellclou.de>
2024-02-20 09:36:53 +0900

    feat: 매칭 로그 view 구현

---
+++ app/my-page/history/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+
+import { Tabs, TabsList, TabsTrigger } from "@/app/_common/shadcn/ui/tabs";
+
+import TempCard from "../_components/TempCard";
+
+function HistoryPage() {
+  const [currentTab, setCurrentTab] = useState("all");
+
+  return (
+    <div className="container max-h-full max-w-2xl space-y-4 bg-gray-50 dark:bg-gray-950">
+      <Tabs
+        defaultValue={currentTab}
+        className="space-y-4"
+        onValueChange={value => setCurrentTab(value)}
+      >
+        <TabsList className="grid w-full grid-cols-4">
+          <TabsTrigger value="all">전체</TabsTrigger>
+          <TabsTrigger value="inprogress">매칭중</TabsTrigger>
+          <TabsTrigger value="finished">종료</TabsTrigger>
+          <TabsTrigger value="failed">실패</TabsTrigger>
+        </TabsList>
+      </Tabs>
+      {/* TODO: currentTab 기준으로 필터링 */}
+      <div className="flex flex-col gap-2">
+        {Array.from({ length: 4 }).map((_, i) => (
+          <TempCard key={i} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HistoryPage;
