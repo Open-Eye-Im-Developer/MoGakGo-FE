0361dd5c3eb2d217cd4a6324d71b99aebbfa9112
yougyung <youkyeongs@naver.com>
2024-02-20 17:48:28 +0900

    feat: Empty 알림 안내 구현

---
+++ app/notification/_components/AnnounceEmptyNotification.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from "tailwind-merge";
+import Lottie from "react-lottie-player";
+import Link from "next/link";
+
+import { buttonVariants } from "@/app/_common/shadcn/ui/button";
+
+import animation from "../_assets/notification.json";
+function AnnounceEmptyNotification() {
+  return (
+    <div className="m-auto flex w-fit flex-col p-2 text-gray-600">
+      <Lottie play animationData={animation} />
+      <span>아직 받은 알람이 없어요</span>
+      <Link
+        href={"/"}
+        className={twMerge(
+          buttonVariants({ variant: "ghost" }),
+          "text-gray-400",
+        )}
+      >
+        활동하러 가기
+      </Link>
+    </div>
+  );
+}
+
+export default AnnounceEmptyNotification;
--- app/notification/_components/NotificationList.tsx
+++ app/notification/_components/NotificationList.tsx
@@ -1,15 +1,7 @@
-import { twMerge } from "tailwind-merge";
-import Lottie from "react-lottie-player";
-import React from "react";
-import Link from "next/link";
-
-import { buttonVariants } from "@/app/_common/shadcn/ui/button";
-
 import { NotificationType } from "../_types/notification";
 import Notification from "../_components/Notification";
-import animation from "../_assets/notification.json";
 import useGetNotifications from "../_api/useGetNotifications";
-
+import AnnounceEmptyNotificaiton from "./AnnounceEmptyNotification";
 function NotificationList() {
   const { data: notificaitons } = useGetNotifications();
 
@@ -24,24 +16,7 @@ function NotificationList() {
             </div>
           ))
       ) : (
-        <div className="m-auto flex w-fit flex-col p-2 text-gray-600">
-          <Lottie
-            loop
-            play
-            animationData={animation}
-            style={{ width: 150, height: 150 }}
-          />
-          <div>아직 받은 알람이 없어요</div>
-          <Link
-            className={twMerge(
-              buttonVariants({ variant: "ghost" }),
-              "text-gray-400",
-            )}
-            href={"/"}
-          >
-            활동하러 가기
-          </Link>
-        </div>
+        <AnnounceEmptyNotificaiton />
       )}
     </>
   );
