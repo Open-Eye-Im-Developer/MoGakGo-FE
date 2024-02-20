d5b41e14d57ca87d49431d8ac81ba0daaa929952
lunarmoon7 <zentechie7@gmail.com>
2024-02-20 11:34:38 +0900

    cleanup: 임시 메시지 파일 JSON 상수화

--- app/project/_components/ProjectCardContainer.tsx
+++ app/project/_components/ProjectCardContainer.tsx
@@ -8,56 +8,10 @@ import {
 } from "@/app/_common/shadcn/ui/tabs";
 
 import useFlip from "../_hooks/useFlip";
+import TEST_MESSAGES from "../_constants/messages";
 import ProjectChatCard from "./ProjectChatCard";
 import ProjectCardFront from "./ProjectCardFront";
 import ProjectCardBack from "./ProjectCardBack";
-
-const messages = [
-  {
-    time: "오후 7:27",
-    content: "안녕하세요?",
-  },
-  {
-    time: "오후 7:29",
-    content: "아 넵 안녕하세요. 미리 와 계시나요?",
-  },
-  {
-    time: "오후 7:29",
-    content: "저는 앞입니다!",
-  },
-  {
-    time: "오후 7:30",
-    content: "어, 저도 카페에 들어와있는데",
-  },
-  {
-    time: "오후 7:30",
-    content: "무슨 색 옷 입고 계시나요?",
-  },
-  {
-    time: "오후 7:30",
-    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
-  },
-  {
-    time: "오후 7:30",
-    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
-  },
-  {
-    time: "오후 7:30",
-    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
-  },
-  {
-    time: "오후 7:30",
-    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
-  },
-  {
-    time: "오후 7:30",
-    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
-  },
-  {
-    time: "오후 7:30",
-    content: "오잉? ㅋㅋㅋ 저 노란색이요!",
-  },
-];
 
 function ProjectCardContainer() {
   const { flipped, handleFlip } = useFlip();
@@ -77,7 +31,7 @@ function ProjectCardContainer() {
         </div>
       </TabsContent>
       <TabsContent value="chat" className="h-full">
-        <ProjectChatCard messages={messages} />
+        <ProjectChatCard messages={TEST_MESSAGES} />
       </TabsContent>
     </Tabs>
   );
