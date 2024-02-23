aa0c837a5fb4bd9536baed9fc861d72cf962cfe6
mibu119 <horang296@gmail.com>
2024-02-23 10:20:39 +0900

    fix: instance 수정

--- app/_common/api/instance.ts
+++ app/_common/api/instance.ts
@@ -1,36 +1,30 @@
 import axios from "axios";
 
-// import { reIssueAccessToken } from "@/app/_common/api/auth";
+const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const SERVER_VERSION = "/api/v1";
-
-const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
 export const instance = axios.create({
   baseURL: `${BASE_URL}${SERVER_VERSION}`,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    Authorization: `Bearer ${accessToken}`,
   },
-  validateStatus: status => status < 500,
 });
 
 instance.interceptors.request.use(
   config => {
-    if (config.headers.ignoreGlobalCatch) {
-      config.headers["Content-Type"] = "application/json";
-      config.headers.Accept = "application/json";
-      config.baseURL = `${BASE_URL}`;
+    // TODO: 회원가입/로그인 merge 후 주석 해제
+    // const accessToken =
+    //   localStorage.getItem("accessToken") ??
+    //   sessionStorage.getItem("accessToken");
 
-      return config;
-    }
+    config.headers.Authorization = `Bearer ${accessToken}`;
 
     return config;
   },
-  (error: Error) => {
-    console.log(error.message);
+  error => {
     return Promise.reject(error);
   },
 );
@@ -41,25 +35,6 @@ instance.interceptors.response.use(
   },
   // 에러 처리
   async error => {
-    const { config, response } = error;
-
-    // 토큰 자동 재발급 필요 외 다른 에러
-    if (
-      config.url === `/auth/reissue` ||
-      response?.status !== 402 ||
-      config.sent
-    ) {
-      return Promise.reject(error);
-    }
-
-    // TODO: reIssueAccessToken 호출 추가 후 주석해제
-    // config.sent = true;
-    // const reissue = await reIssueAccessToken();
-
-    // if (reissue.data.accessToken) {
-    //   config.headers.Authorization = `Bearer ${reissue.data.accessToken}`;
-    // }
-
-    return instance(config);
+    return Promise.reject(error);
   },
 );
