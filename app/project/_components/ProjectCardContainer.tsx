d17e8d292c3c831ba3db4cd7d768d18cc7f67670
lunarmoon7 <zentechie7@gmail.com>
2024-02-25 19:34:50 +0900

    cleanup: useGetRequestListQuery Hook 호출 위치 변경
    - 프로젝트 요청 목록은 프로젝트 생성자가 본인이 아닌 경우는 볼 수 없어야 합니다.
    - 즉, 카드 뒤집기 기능이 막혀야 합니다.
    - 그러므로 ProjectCardBack에서 호출하던 Hook의 위치를 ProjectCardContainer로 변경했습니다.

--- app/project/_components/ProjectCardContainer.tsx
+++ app/project/_components/ProjectCardContainer.tsx
@@ -8,18 +8,21 @@ import {
   TabsTrigger,
 } from "@/app/_common/shadcn/ui/tabs";
 
+import useGetRequestListQuery from "../_hooks/useGetRequestListQuery";
 import useFlip from "../_hooks/useFlip";
 import TEST_MESSAGES from "../_constants/messages";
 import ProjectChatCard from "./ProjectChatCard";
 import ProjectCardFront from "./ProjectCardFront";
 import ProjectCardBack from "./ProjectCardBack";
 
+// TODO: 제안 카드를 생성한 생성자가 아닌 경우, 제안 카드의 뒷면을 보지 못하도록(flip이 불가하도록) 처리 & 서버에서 프로젝트 id 받아오기
 function ProjectCardContainer() {
   const { flipped, handleFlip } = useFlip();
+  const { data } = useGetRequestListQuery(85);
 
   return (
     <Tabs defaultValue="card" className="h-[550px] w-[330px] sm:w-[450px]">
-      <TabsList className="grid w-full grid-cols-2">
+      <TabsList className="glass-morphism grid w-full grid-cols-2">
         <TabsTrigger value="card">Card</TabsTrigger>
         <TabsTrigger value="chat">Chat</TabsTrigger>
       </TabsList>
@@ -31,7 +34,7 @@ function ProjectCardContainer() {
           )}
         >
           <ProjectCardFront onRotate={handleFlip} />
-          <ProjectCardBack onRotate={handleFlip} />
+          <ProjectCardBack requestList={data} onRotate={handleFlip} />
         </div>
       </TabsContent>
       <TabsContent value="chat" className="h-full">
