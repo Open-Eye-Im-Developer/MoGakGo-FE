33c56e3fa02b99d32559322cad97499774e8f172
yougyung <youkyeongs@naver.com>
2024-02-26 09:28:33 +0900

    [feat] 알람 기능 구현 (#31)
    * feat: Notification 컴포넌트 구현
    
    * feat: StackNavigator 구현
    
    * feat: alarm-list구현
    
    * refactor: notification list 분리
    
    * feat: Empty 알림 안내 구현
    
    * feat: push 알람 요청
    
    * fix: showModal 변수명 수정
    
    * fix: convention 수정
    
    * feat: api 연결
    
    * fix: permission check 수정
    
    * fix: 리뷰 반영 수정
    
    * chore: 줄바꿈 추가
    
    * docs: .env.sample 추가

---
+++ app/notification/_components/Notification.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import relativeTime from "dayjs/plugin/relativeTime";
+import dayjs from "dayjs";
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/app/_common/shadcn/ui/avatar";
+
+import { NotificationType } from "../_types/notification";
+
+interface NotificationProps {
+  notification: NotificationType;
+}
+
+function Notification({ notification }: NotificationProps) {
+  dayjs.extend(relativeTime);
+
+  const { sender, data } = notification;
+  const { name, profile_image } = sender;
+  const { detail, created_at } = data;
+
+  return (
+    <div className="m-2 flex cursor-pointer justify-between border-b p-2 px-3">
+      <div className="flex gap-4">
+        <Avatar>
+          <AvatarImage src={profile_image} />
+          <AvatarFallback>{name.charAt(0)}</AvatarFallback>
+        </Avatar>
+        <div>
+          <div className="font-medium">{name}</div>
+          <span className="text-gray-500">{detail}</span>
+        </div>
+      </div>
+      <div className="min-w-fit text-center text-sm text-gray-500">
+        <div>{dayjs(created_at).fromNow()}</div>
+      </div>
+    </div>
+  );
+}
+export default Notification;
---
+++ app/notification/_components/NotificationList.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { NotificationType } from "../_types/notification";
+import Notification from "../_components/Notification";
+import useGetNotifications from "../_api/useGetNotifications";
+import AnnounceEmptyNotificaiton from "./AnnounceEmptyNotification";
+
+function NotificationList() {
+  const { data: notificaitons } = useGetNotifications();
+
+  return (
+    <>
+      {notificaitons?.length ? (
+        [...notificaitons]
+          .reverse()
+          .map((notification: NotificationType, index: number) => (
+            <Notification notification={notification} key={index} />
+          ))
+      ) : (
+        <AnnounceEmptyNotificaiton />
+      )}
+    </>
+  );
+}
+
+export default NotificationList;
