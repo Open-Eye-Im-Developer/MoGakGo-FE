d7d4964daadb6ad015e9199d6efc950648ba0128
mibu119 <horang296@gmail.com>
2024-02-26 19:02:36 +0900

    fix: modal store 수정

--- app/_common/store/useModalStore.ts
+++ app/_common/store/useModalStore.ts
@@ -2,15 +2,19 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import { create } from "zustand";
 
 interface ModalState {
-  open: boolean;
-  setOpen: (open: boolean) => void;
+  signup: boolean;
+  authLocation: boolean;
+  setSignupOpen: (open: boolean) => void;
+  setAuthLocationOpen: (open: boolean) => void;
 }
 
 export const useModalStore = create(
   persist<ModalState>(
     set => ({
-      open: false,
-      setOpen: (open: boolean) => set({ open }),
+      signup: false,
+      authLocation: false,
+      setSignupOpen: (open: boolean) => set({ signup: open }),
+      setAuthLocationOpen: (open: boolean) => set({ authLocation: open }),
     }),
     {
       name: "modal-store",
