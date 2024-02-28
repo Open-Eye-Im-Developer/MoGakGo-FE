9c7420c83dcd3f762a515bc267c9d2f9270ea9ea
yoon <73841260+bgyoons@users.noreply.github.com>
2024-02-28 10:42:09 +0900

    [feat] 프로젝트/프로필 카드 조회 기능 개발 (#66)
    * feat: 지역별 프로젝트, 프로필 카드 api 생성
    
    * feat: 지도 화면에서 프로젝트, 프로필 카드 조회 불러오기
    
    * feat: 프로젝트 카드 컴포넌트에 프로젝트 정보 props 생성
    
    * feat: 프로젝트 카드 리스트 타입 추가
    
    * feat: 미팅 시간 포멧팅 유틸 추가
    
    * test: 미팅 시간 포멧팅 유틸 테스트 함수 추가
    
    * feat: 받아온 프로젝트 정보 props 연결
    
    * refactor: 받아온 props 구조분해할당으로 리팩토링
    
    * feat: 프로젝트 카드 상태 추가
    
    * style: 확대 애니메이션이 1초로 카드 리스트 보여주기까지 딜레이 1초 추가
    
    * refactor: 프로젝트 타입 공통 타입 파일에 분리
    
    * refactor: import문 절대경로로 변경
    
    * feat: 프로젝트 카드 리스트 조회 api 생성
    
    * refactor: creator 타입 프로필로 분리
    
    * feat: 응답 데이터 제네릭 생성
    
    * refactor: response 파일 이름 개발 컨벤션에 맞게 변경
    
    * feat: 프로필 카드 리스트 조회 api 생성
    
    * fix: Project 타입 분리로 import 경로 변경
    
    * cleanup: api routing fetch 함수 삭제
    
    * cleanup: 사용 안 하는 함수 제거 및 TODO 주석 추가
    
    * feat: 프로젝트, 프로필 카드 조회 api 쿼리 추가
    
    * feat: 프로젝트, 프로필 카드 불러오는 훅 구현
    
    * fix: region 값이 있을 때만 쿼리 실행
    
    * feat: 데이터 불러오기 및 목업데이터 삭제
    
    * feat: 지역 이름 유효한 쿼리로 변경하는 로직 추가
    
    * fix: 쿼리 중 에러인 경우 분기 처리
    
    * fix: 법정구역코드 타입 숫자로 변경
    
    * style: 조회할 수 있는 카드 리스트가 없는 경우 추가
    
    * feat: 카드 리스트가 없을 때 돌아가기 버튼 이벤트 추가
    
    * cleanup: lint 적용
    
    * refactor: 반복되는 줌 아웃 코드 함수로 분리
    
    * fix: 같은 지역을 연속으로 조회하는 경우 애니메이션이 실행 안되는 버그
    
    * refactor: 카드 리스트가 없는 경우 컴포넌트로 분리
    
    * refactor: 카드 리스트 캐러샐 컴포넌트로 분리
    
    * fix: 미팅 시간을 dayjs 이용해 구하는 로직으로 변경
    
    * fix: api url / 붙이는 것으로 통일

--- app/_common/api/project.ts
+++ app/_common/api/project.ts
@@ -1,6 +1,7 @@
 import { AxiosError } from "axios";
 
-import { ResponseError } from "../types/response.types";
+import { ResponseData, ResponseError } from "../types/response";
+import { Project } from "../types/project";
 import { instance } from "./instance";
 
 interface RegionRank {
@@ -11,7 +12,22 @@ export const getRank = async (): Promise<
   RegionRank | ResponseError | undefined
 > => {
   try {
-    const { data } = await instance.get<RegionRank>("projects/density/rank");
+    const { data } = await instance.get<RegionRank>("/projects/density/rank");
+    return data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error.response?.data;
+    }
+  }
+};
+
+export const getProjectCard = async (
+  region: string,
+): Promise<ResponseData<Project> | ResponseError | undefined> => {
+  try {
+    const { data } = await instance.get<ResponseData<Project>>(
+      `/projects/${region}?cursorId=1&pageSize=5&sortOrder=ASC`,
+    );
     return data;
   } catch (error) {
     if (error instanceof AxiosError) {
