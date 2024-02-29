e9c78a432306a8aeccb009ebf8ff1ae9f6335166
lunarmoon7 <zentechie7@gmail.com>
2024-02-29 17:31:36 +0900

    feat: 참가 요청 목록 불러오는 query 훅 실행 조건 추가
    - 프로젝트 카드를 생성한 사용자와 현재 접속한 사용자가 동일할 경우에만, query가 실행되도록 enabled 옵션을 설정했습니다.

--- app/project/_hooks/useGetRequestListQuery.ts
+++ app/project/_hooks/useGetRequestListQuery.ts
@@ -1,7 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 
-// TODO: 프로젝트 카드 생성자가 아닌 사용자가 요청한 프로젝트 목록을 가져오지 못하도록 예외 처리
-function useGetRequestListQuery(id: number) {
+import { useAuthStore } from "@/app/_common/store/useAuthStore";
+
+function useGetRequestListQuery(id: number, creatorId: number) {
+  const { user } = useAuthStore();
+
   const queryFunction = async () => {
     const response = await fetch(`/api/project/request/find?id=${id}`);
     const data: RequestListResponseData = await response.json();
@@ -11,7 +14,7 @@ function useGetRequestListQuery(id: number) {
   return useQuery({
     queryKey: ["requestList"] as const,
     queryFn: queryFunction,
-    refetchInterval: 7000,
+    enabled: user.id === creatorId,
   });
 }
 
