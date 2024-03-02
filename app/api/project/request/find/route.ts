7b0d1ff1f05207b58ce003182e309dde11abe7b4
lunarmoon7 <zentechie7@gmail.com>
2024-03-02 18:48:47 +0900

    feat: 쿼리 스트링 추가
    - API 호출 시, cursorId를 쿼리 스트링으로 추가했습니다.

--- app/api/project/request/find/route.ts
+++ app/api/project/request/find/route.ts
@@ -3,11 +3,13 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const id = request.nextUrl.searchParams.get("id");
+  const cursorId = request.nextUrl.searchParams.get("cursorId");
 
   // TODO: 프로젝트 아이디 받아와서 호출하기 (현재는 임시로 64 또는 65로 설정)
   const url = new URL(`http://3.38.76.76:8080/api/v1/projects/${id}/requests`);
   const params = {
     pageSize: "5",
+    cursorId: cursorId!,
   };
   url.search = new URLSearchParams(params).toString();
 
