448ab4fa9428c8811f10e2cdce4c8875fc86ff99
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-03-04 00:04:44 +0900

    feat: 프로젝트, 프로필 리스트 불러오기 무한 스크롤 구현

--- app/(map)/_api/useGetCardList.ts
+++ app/(map)/_api/useGetCardList.ts
@@ -1,44 +1,89 @@
-import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { getProjectCard } from "@/app/_common/api/project";
 import { getProfileCard } from "@/app/_common/api/profile";
 
 import { formatRegionName } from "../_utils/formatRegionName";
 
-// TODO: 페이지네이션 구현
 const useGetCardList = (regionCode: number) => {
   const region = formatRegionName(regionCode).toUpperCase();
+  const [isGetProfile, setIsGetProfile] = useState(false);
 
-  const projectResponse = useQuery({
+  const {
+    data: projectList,
+    isLoading: isProjectLoading,
+    fetchNextPage: fetchProject,
+    hasNextPage: hasNextProject,
+    isFetchingNextPage: isFetchingProject,
+  } = useInfiniteQuery({
     queryKey: ["project-list", region] as const,
-    queryFn: () => getProjectCard(region),
+    queryFn: ({ pageParam = 0 }) => {
+      return getProjectCard({ region, cursorId: pageParam });
+    },
+    initialPageParam: 0,
+    getNextPageParam: lastPage => {
+      if (!lastPage.hasNext) {
+        setIsGetProfile(true);
+        return;
+      }
+      const cursorId = lastPage.data[lastPage.data.length - 1].projectId;
+      return cursorId;
+    },
     enabled: !!region,
   });
 
-  const profileResponse = useQuery({
+  const {
+    data: profileList,
+    isLoading: isProfileLoading,
+    fetchNextPage: fetchProfile,
+    hasNextPage: hasNextProfile,
+    isFetchingNextPage: isFetchingProfile,
+  } = useInfiniteQuery({
     queryKey: ["profile-list", region] as const,
-    queryFn: () => getProfileCard(region),
-    enabled: !!region,
+    queryFn: ({ pageParam = 0 }) => {
+      return getProfileCard({ region, cursorId: pageParam });
+    },
+    initialPageParam: 0,
+    getNextPageParam: lastPage => {
+      if (!lastPage.hasNext) {
+        setIsGetProfile(false);
+        return;
+      }
+      const cursorId = lastPage.data[lastPage.data.length - 1].id;
+      return cursorId;
+    },
+    enabled: !!region && isGetProfile,
   });
 
-  if (
-    projectResponse.isLoading ||
-    !projectResponse.data ||
-    profileResponse.isLoading ||
-    !profileResponse.data
-  ) {
-    return { data: undefined, isLoading: true, isError: false, error: null };
+  const defaultValue = {
+    isProjectLoading,
+    fetchProject,
+    hasNextProject,
+    isFetchingProject,
+    isProfileLoading,
+    fetchProfile,
+    hasNextProfile,
+    isFetchingProfile,
+  };
+
+  if (isProjectLoading || !projectList) {
+    return {
+      cardList: { projectList: [], profileList: [] },
+      ...defaultValue,
+    };
   }
 
-
-  const { data: projectList } = projectResponse.data;
-  const { data: profileList } = profileResponse.data;
+  if (isProfileLoading || !profileList) {
+    return {
+      cardList: { projectList, profileList: [] },
+      ...defaultValue,
+    };
+  }
 
   return {
-    data: { projectList, profileList },
-    isLoading: false,
-    isError: false,
-    error: null,
+    cardList: { projectList, profileList },
+    ...defaultValue,
   };
 };
 
