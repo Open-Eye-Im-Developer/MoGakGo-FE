f2856dc64a175bd7bf492750739177f95caa31a0
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-03-04 19:13:11 +0900

    [feat] 프로젝트 요청 목록 무한스크롤 적용 (#83)
    * refactor: 인터페이스 수정
    
    * feat: 무한 스크롤 커스텀 훅 생성
    
    * feat: 쿼리 스트링 추가
    
    - API 호출 시, cursorId를 쿼리 스트링으로 추가했습니다.
    
    * feat: forwardRef 적용
    
    * feat: 무한 스크롤 적용 & API 응답 state로 관리 & ref 전달
    
    * feat: queryKey에 state 추가
    
    - cursorId가 변경될 때 마다, useQuery가 호출됩니다.
    
    * refactor: 코드 개선
    
    - 역할에 맞는 변수명으로 변경
    - `null` early check
    
    * refactor: 변수 명 오타 수정

--- app/project/_hooks/useGetRequestListQuery.ts
+++ app/project/_hooks/useGetRequestListQuery.ts
@@ -1,22 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { useAuthStore } from "@/app/_common/store/useAuthStore";
-
-// TODO: 프로젝트 카드 참가 요청이 왔을 때만, query가 호출되도록 로직 추가
-function useGetRequestListQuery(id: number, creatorId: number) {
-  const { user } = useAuthStore();
-
+// TODO: 프로젝트 카드 생성자가 아닌 사용자가 요청한 프로젝트 목록을 가져오지 못하도록 예외 처리
+function useGetRequestListQuery(id: number, cursorId: number) {
   const queryFunction = async () => {
-    const response = await fetch(`/api/project/request/find?id=${id}`);
+    const response = await fetch(
+      `/api/project/request/find?id=${id}&cursorId=${cursorId}`,
+    );
     const data: RequestListResponseData = await response.json();
     return data;
   };
 
   return useQuery({
-    queryKey: ["requestList"] as const,
+    queryKey: ["requestList", cursorId] as const,
     queryFn: queryFunction,
-    enabled: user.id === creatorId,
-    refetchInterval: 5000,
   });
 }
 
