5d21131720dceeed91e2d4d157a936148ae81984
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-03-04 12:14:11 +0900

    style: 찜하기 버튼 스타일 변경

--- app/project/_components/ButtonFollow.tsx
+++ app/project/_components/ButtonFollow.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { IconHeartFilled } from "@tabler/icons-react";
 
 import { Button } from "@/app/_common/shadcn/ui/button";
 
 function ButtonFollow() {
   return (
-    <Button className="mt-0 flex items-center justify-center p-2 text-[#FF0000]">
+    <Button className="mt-0 flex items-center justify-center bg-[#F9679C] p-2 text-white">
       <IconHeartFilled />
     </Button>
   );
