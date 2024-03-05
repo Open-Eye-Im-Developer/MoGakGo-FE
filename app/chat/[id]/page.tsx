c7cd8b0e688c428b6ddcb6fc998ba2e275414c5a
lunarmoon7 <zentechie7@gmail.com>
2024-03-05 13:57:02 +0900

    feat: 이전 채팅 내용 가져오는 로직 추가
    - server side fetch를 사용했습니다.

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -2,7 +2,28 @@ import StackNavigator from "../../_common/components/StackNavigator";
 import ProjectInfo from "./_components/ProjectInfo";
 import Profile from "./_components/Profile";
 import MessageContainer from "./_components/MessageContainer";
-function ChattingRoomPage() {
+
+// TODO 배포 도메인 주소로 변경하여 API 호출
+async function getPreviousChatMessageList(chatRoomId: string) {
+  const response = await fetch(
+    `http://localhost:3000/api/chat?chatRoomId=${chatRoomId}`,
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      chatMessageList: data.data,
+    },
+  };
+}
+
+// TODO: chatMessageList를 받아와서 MessageContainer에 props로 넘긴다.
+async function ChattingRoomPage({ params }: { params: { id: string } }) {
+  const {
+    props: { chatMessageList },
+  } = await getPreviousChatMessageList(params.id);
+  // TODO: husky로 인해 커밋이 안되므로 임시 추가됨. 삭제 필요
+  console.log(chatMessageList)
   return (
     <>
       <StackNavigator
@@ -13,7 +34,7 @@ function ChattingRoomPage() {
         }
       />
       <ProjectInfo />
-      <MessageContainer />
+      <MessageContainer chatRoomId={params.id} />
     </>
   );
 }
