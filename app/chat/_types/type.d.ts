8863ef8badcbf3d9c59a79241d0836f9d3b17541
lunarmoon7 <zentechie7@gmail.com>
2024-03-05 13:58:44 +0900

    cleanup: 사용하지 않는 interface 삭제

--- app/chat/_types/type.d.ts
+++ app/chat/_types/type.d.ts
@@ -1,35 +1,8 @@
-interface ChatRoom {
-  projectId: number;
-  chatRoomId: string;
-  lastMessage: string;
-  lastMessageCreatedAt: string;
-  status: string;
-  profiles: {
-    userId: number;
-    username: string;
-    avatarUrl: string;
-  }[];
-}
 
 interface ChatMessage {
+  id: number
   messageType: string;
   senderId: number;
   message: string;
   createdAt: string;
-}
-
-interface ChatRoomDetail {
-  project: {
-    meetDetail: string;
-    meetStartTime: string;
-    meetEndTime: string;
-  };
-  data: ChatMessage[];
-}
-
-interface WebSocketMessage {
-  messageType: string;
-  senderId: number;
-  message: string;
-  chatRoomId: string;
 }
