2af46ae54617c5f8b203fd372de33a25434090e1
yoon <73841260+bgyoons@users.noreply.github.com>
2024-03-05 10:25:59 +0900

    [feat] 지역별 카드 조회 페이지네이션 로직 구현 (#93)
    * refactor: 지역 이름 반환하는 유틸 사용
    
    * feat: 법정구역코드 통해 지역 이름 반환하는 유틸 생성
    
    * test: 법정구역코드 통해 지역 이름 반환하는 유틸 테스트 생성
    
    * fix: 프로젝트, 프로필 카드 조회에서 에러 처리 필요 없으므로 에러 핸들링 코드 삭제
    
    * refactor: 밀도 순위에서 에러 처리 필요 없으므로 에러 핸들링 코드 삭제
    
    * feat: 밀도 순위 불러올 때 로딩 컴포넌트 추가
    
    * fix: 구역별 프로젝트/프로필 카드 초기 조회 때 cursorId 안 들어가도록 쿼리 수정
    
    * feat: 프로젝트, 프로필 리스트 불러오기 무한 스크롤 구현
    
    * fix: 무한 렌더링으로 useState에서 useRef로 변경
    
    * fix: 프로젝트, 프로필 카드 조회 데이터 반환값 변경
    
    * fix: 데이터 반환값 분기 처리로 조건문에서 cardList 논리곱 삭제
    
    * feat: 캐러샐 api 연결해서 데이터 패치 로직 구현
    
    * refactor: 캐러샐 api 상태 이름 명시적으로 변경
    
    * feat: 프로필 카드 아이템 컴포넌트 구현
    
    * feat: 프로필 카드 컴포넌트 리스트에 적용
    
    * style: 필요없는 스타일 제거
    
    * refactor: 구조분해할당으로 props 선언
    
    * style: 찜하기 버튼 스타일 변경
    
    * style: 로딩스패너가 화면을 가리도록 스타일 수정
    
    * refactor: 로딩스패너 className props 추가
    
    * refactor: 로딩스패너 구조 변경으로 className props 스타일 추가
    
    * style: 로딩스피너 트랜지션 추가
    
    * refactor: early return문으로 변경
    
    * feat: 내 위치 조회 후 리스트 상태 변경

---
+++ app/(map)/_components/ProfileCardItem.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import ButtonFollow from "@/app/project/_components/ButtonFollow";
+import { Progress } from "@/app/_common/shadcn/ui/progress";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/app/_common/shadcn/ui/card";
+import { Badge as Tag } from "@/app/_common/shadcn/ui/badge";
+
+import { Creator } from "@/app/_common/types/profile";
+
+interface Props {
+  profile: Creator;
+}
+
+function ProfileCardItem({ profile }: Props) {
+  const {
+    username,
+    githubId,
+    avatarUrl,
+    githubUrl,
+    bio,
+    jandiRate,
+    achievementTitle,
+    developLanguages,
+    wantedJobs,
+  } = profile;
+
+  return (
+    <div className="h-[550px] w-[330px] sm:w-[450px]">
+      <div className="h-full">
+        <div className="relative h-full">
+          <Card className="border-none shadow-md">
+            <CardHeader className="px-5 pt-4">
+              <CardDescription className="flex justify-end text-lg font-bold text-black">
+                <Link href={githubUrl} target="_blank">
+                  @{githubId}
+                </Link>
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col items-center gap-10">
+              <div className="flex flex-col items-center gap-5">
+                <div className="relative overflow-hidden rounded-xl shadow-lg">
+                  <Image
+                    src={avatarUrl}
+                    alt="프로필 이미지"
+                    width={150}
+                    height={150}
+                  />
+                </div>
+                <div className="flex flex-col items-center gap-[6px]">
+                  <div className="flex flex-col items-center gap-1 p-1">
+                    <h1 className="text-xl font-bold">{username}</h1>
+                    <h3 className="text-xs text-[#F76A6A]">
+                      {achievementTitle}
+                    </h3>
+                  </div>
+                  <p className="text-[#868686]">{bio}</p>
+                  <div className="flex flex-wrap items-center justify-center gap-1">
+                    {wantedJobs.map(job => (
+                      <Tag key={job}>{job}</Tag>
+                    ))}
+                  </div>
+                  <div className="flex flex-wrap items-center justify-center gap-1">
+                    {developLanguages.map(job => (
+                      <Tag key={job}>{job}</Tag>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="flex w-full items-center justify-center">
+                <Image
+                  src="/images/grass.png"
+                  alt="잔디력"
+                  width={50}
+                  height={50}
+                />
+                <div className="mr-10 w-40">
+                  <Progress value={jandiRate} />
+                  <span className="text-xs text-[#868686]">
+                    {jandiRate * 100}%
+                  </span>
+                </div>
+              </div>
+            </CardContent>
+            <CardFooter className="flex justify-end">
+              <ButtonFollow />
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCardItem;
--- app/_common/api/project.ts
+++ app/_common/api/project.ts
@@ -1,6 +1,4 @@
-import { AxiosError } from "axios";
-
-import { ResponseData, ResponseError } from "../types/response";
+import { ResponseData } from "../types/response";
 import { Project } from "../types/project";
 import { instance } from "./instance";
 
@@ -8,30 +6,23 @@ interface RegionRank {
   densityRankByRegion: string[];
 }
 
-export const getRank = async (): Promise<
-  RegionRank | ResponseError | undefined
-> => {
-  try {
-    const { data } = await instance.get<RegionRank>("/projects/density/rank");
-    return data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      return error.response?.data;
-    }
-  }
+export const getRank = async (): Promise<RegionRank> => {
+  const { data } = await instance.get<RegionRank>("/projects/density/rank");
+  return data;
 };
 
-export const getProjectCard = async (
-  region: string,
-): Promise<ResponseData<Project> | ResponseError | undefined> => {
-  try {
-    const { data } = await instance.get<ResponseData<Project>>(
-      `/projects/${region}?cursorId=1&pageSize=5&sortOrder=ASC`,
-    );
-    return data;
-  } catch (error) {
-    if (error instanceof AxiosError) {
-      return error.response?.data;
-    }
-  }
+interface ProjectCardRequest {
+  region: string;
+  cursorId?: number;
+}
+
+export const getProjectCard = async ({
+  region,
+  cursorId,
+}: ProjectCardRequest) => {
+  const query = `${cursorId ? `cursorId=${cursorId}&` : ""}pageSize=5&sortOrder=ASC`;
+  const { data } = await instance.get<ResponseData<Project>>(
+    `/projects/${region}?${query}`,
+  );
+  return data;
 };
