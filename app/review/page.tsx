757b3e9f5c642fa16de495d780147b96d4b82db1
mibu119 <horang296@gmail.com>
2024-03-06 10:05:12 +0900

    chore: useSearchParams 사용 시  필요 suspense 추가

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from "react";
+
+import LoadingSpinner from "../_common/components/LoadingSpinner";
 import ReviewForm from "./_components/ReviewForm";
 
 function ReviewPage() {
   return (
-    <main className="p-4">
-      <ReviewForm />
-    </main>
+    <Suspense fallback={<LoadingSpinner />}>
+      <main className="p-4">
+        <ReviewForm />
+      </main>
+    </Suspense>
   );
 }
 
