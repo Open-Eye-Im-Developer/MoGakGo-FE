0b6cd0425358a1bca2d01ea1363c043be9bad32d
yoon <73841260+bgyoons@users.noreply.github.com>
2024-03-06 12:02:05 +0900

    [fix] 로그인 시 사용자 정보 전역 스토어 저장 로직 구현 (#111)
    * fix: 토큰 여부에 따라 유저 정보 조회하도록 파라미터 추가
    
    * feat: 엑세스 토큰이 있는 경우에 유저 정보 불러와 전역 상태에 저장

--- app/_common/hooks/useSearchTokens.ts
+++ app/_common/hooks/useSearchTokens.ts
@@ -3,6 +3,10 @@
 import { useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
+import { useQuerySignUpUser } from "@/app/signup/_hooks/useQuerySignUpUser";
+
+import { useAuthStore } from "../store/useAuthStore";
+
 const saveNewTokens = (newAccessToken: string, newRefreshToken: string) => {
   sessionStorage.setItem("accessToken", newAccessToken);
   sessionStorage.setItem("refreshToken", newRefreshToken);
@@ -16,6 +20,8 @@ const saveUpdatedTokens = (newAccessToken: string, newRefreshToken: string) => {
 export const useSearchTokens = (type: "local" | "session") => {
   const newAccessToken = useSearchParams().get("accessToken") ?? "";
   const newRefreshToken = useSearchParams().get("refreshToken") ?? "";
+  const { data } = useQuerySignUpUser(!!newAccessToken);
+  const { setUser, user } = useAuthStore();
 
   useEffect(() => {
     if (!newAccessToken || !newRefreshToken) return;
@@ -25,7 +31,8 @@ export const useSearchTokens = (type: "local" | "session") => {
     } else {
       saveUpdatedTokens(newAccessToken, newRefreshToken);
     }
-  }, [newAccessToken, newRefreshToken, type]);
+    if (data && !user) setUser(data);
+  }, [newAccessToken, newRefreshToken, type, data]);
 
   return {
     newAccessToken,
