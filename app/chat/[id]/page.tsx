1f419cce4abe43d0ff6e7a711be1640fdc45e2fb
lunarmoon7 <zentechie7@gmail.com>
2024-03-07 21:18:51 +0900

    cleanup: server fetch 삭제

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,26 +1,8 @@
 import StackNavigator from "../../_common/components/StackNavigator";
-import ProjectInfo from "./_components/ProjectInfo";
 import Profile from "./_components/Profile";
 import MessageContainer from "./_components/MessageContainer";
 
-// TODO 배포 도메인 주소로 변경하여 API 호출
-async function getPreviousChatMessageList(chatRoomId: string) {
-  const response = await fetch(
-    `http://localhost:3000/api/chat?chatRoomId=${chatRoomId}`,
-  );
-  const { data } = await response.json();
-
-  return {
-    props: {
-      chatMessageList: data,
-    },
-  };
-}
-
 async function ChattingRoomPage({ params }: { params: { id: string } }) {
-  const {
-    props: { chatMessageList },
-  } = await getPreviousChatMessageList(params.id);
   return (
     <main>
       <StackNavigator
@@ -30,8 +12,8 @@ async function ChattingRoomPage({ params }: { params: { id: string } }) {
           </Profile>
         }
       />
-      <ProjectInfo chatRoomId={params.id} />
-      <MessageContainer chatRoomId={params.id} prevChatMessageList={chatMessageList} />
+      {/* <ProjectInfo chatRoomId={params.id} /> */}
+      <MessageContainer chatRoomId={params.id} />
     </main>
   );
 }
