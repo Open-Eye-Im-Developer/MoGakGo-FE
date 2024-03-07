23272b1d4299740324de76ae980f749354a19c78
lunarmoon7 <zentechie7@gmail.com>
2024-03-07 13:12:57 +0900

    feat: API 쿼리 스트링 추가
    - pageSize가 필수옵션이므로 추가했습니다.

--- app/chat/_api/index.ts
+++ app/chat/_api/index.ts
@@ -2,7 +2,9 @@ import { instance } from "@/app/_common/api/instance";
 
 import { ChatType } from "../_types/chat";
 
+// TODO: 무한 스크롤을 적용할 시, cursorId를 인자로 받아와서 query에 추가
 export const getChats = async () => {
-  const { data } = await instance.get<ChatType[]>("/chat");
+  const query = `pageSize=5`;
+  const { data } = await instance.get<ChatType[]>(`/chat?${query}`);
   return data;
 };
