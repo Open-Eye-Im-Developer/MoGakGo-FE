35450fdb37735d69d6b57e2d584fec572f4677c8
yoon <73841260+bgyoons@users.noreply.github.com>
2024-03-08 16:07:05 +0900

    [chore] react-query devtools 설치 (#129)
    * [chore] react-query devtools 설치
    
    * feat: ReactQueryDevtools 최상단에 삽입

--- app/_common/components/ClientProvider.tsx
+++ app/_common/components/ClientProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ReactNode } from "react";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const ClientProvider = ({
@@ -19,7 +20,10 @@ const ClientProvider = ({
   });
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
   );
 };
 
