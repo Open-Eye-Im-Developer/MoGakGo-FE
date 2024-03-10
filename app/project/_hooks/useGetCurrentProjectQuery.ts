1aec78f89413ec565c30983459b3b0297085243f
lunarmoon7 <zentechie7@gmail.com>
2024-03-10 16:35:36 +0900

    feat: 현재 생성한 프로젝트 가져오는 query hook 생성

---
+++ app/project/_hooks/useGetCurrentProjectQuery.ts
@@ -0,0 +1,34 @@
+import { AxiosError } from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+import { useAuthStore } from "@/app/_common/store/useAuthStore";
+import { instance } from "@/app/_common/api/instance";
+
+// TODO: axios 400 이상 코드를 어떻게 useQuery의 error로 넘기는지
+function useGetCurrentProjectQuery() {
+  const { user } = useAuthStore();
+
+  const getCurrentProject = async () => {
+    try {
+      const { data } = await instance.get(`/projects/${user!.id}/info`);
+      return data;
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        return { response: error.response?.data };
+      }
+    }
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["current-project"],
+    queryFn: getCurrentProject,
+    staleTime: Infinity,
+    enabled: user !== null,
+    throwOnError: false,
+  });
+
+  if (isLoading || !data) return { project: null };
+  return { project: data.response };
+}
+
+export default useGetCurrentProjectQuery;
