ef65f3228eed150c667ceaedbe0eb7dce5411eac
lunarmoon7 <zentechie7@gmail.com>
2024-03-10 16:35:54 +0900

    style: 프로젝트 카드 생성 여부에 따른 렌더링 분기 처리

---
+++ app/project/_components/ProjectManageSection.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+
+import useGetCurrentProjectQuery from "../_hooks/useGetCurrentProjectQuery";
+import ProjectCreateDialog from "./ProjectCreateDialog";
+import ProjectCardContainer from "./ProjectCardContainer";
+
+function ProjectManageSection() {
+  const { project } = useGetCurrentProjectQuery();
+
+  return (
+    <main className="flex h-full w-full items-center justify-center">
+      {project && project.length === 0 && (
+        <div className="flex h-[300px] flex-col items-center justify-between rounded-xl border-4 border-dotted p-10">
+          <header className="space-y-3 text-center text-[#7b7b7b]">
+            <h1 className="text-2xl font-bold">생성한 프로젝트가 없어요!</h1>
+            <h2>프로젝트를 새로 생성해주세요.</h2>
+          </header>
+          <ProjectCreateDialog />
+        </div>
+      )}
+      {project && project.length > 0 && (
+        <ProjectCardContainer project={project[0]} />
+      )}
+    </main>
+  );
+}
+
+export default ProjectManageSection;
