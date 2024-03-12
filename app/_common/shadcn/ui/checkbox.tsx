164fc1202d81b00cf94af9c232e9961f06ada935
mibu119 <horang296@gmail.com>
2024-03-12 11:43:58 +0900

    style: shadcn 컴포넌트 스타일 수정
    style: shadcn input variants 수정
    
    [refactor] 브랜드 컬러 tailwind config 설정 (#157)
    
    * refactor: 브랜드 컬러 config 설정
    
    * refactor: camelCase 적용
    
    style: progress shadcn 스타일 변경
    
    style: dialog shadcn 스타일 변경
    
    style: drawer 컴포넌트 스타일 변경
    
    refactor: shadow-neo-t로 수정
    
    style: checkbox shadcn 컴포넌트 스타일 변경

--- app/_common/shadcn/ui/checkbox.tsx
+++ app/_common/shadcn/ui/checkbox.tsx
@@ -1,21 +1,37 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Check } from "lucide-react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
+import * as React from "react";
+import { Check } from "lucide-react";
+import { VariantProps, cva } from "class-variance-authority";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 
-import { cn } from "@/app/_common/shadcn/utils"
+import { cn } from "@/app/_common/shadcn/utils";
+
+const CheckboxVariant = cva(
+  "peer h-4 w-4 shrink-0 rounded-sm border border-black shadow-neo-thin ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=checked]:text-slate-50 dark:border-slate-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900",
+  {
+    variants: {
+      checkedBorder: {
+        default:
+          "data-[state=checked]:border-black dark:data-[state=checked]:border-white",
+        white:
+          "data-[state=checked]:border-white dark:data-[state=checked]:border-black",
+      },
+    },
+    defaultVariants: {
+      checkedBorder: "default",
+    },
+  },
+);
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> &
+    VariantProps<typeof CheckboxVariant>
+>(({ className, checkedBorder, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
-    className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-slate-200 border-slate-900 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=checked]:text-slate-50 dark:border-slate-800 dark:border-slate-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900",
-      className
-    )}
+    className={cn(CheckboxVariant({ checkedBorder }), className)}
     {...props}
   >
     <CheckboxPrimitive.Indicator
@@ -24,7 +40,7 @@ const Checkbox = React.forwardRef<
       <Check className="h-4 w-4" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
-export { Checkbox }
+export { Checkbox };
