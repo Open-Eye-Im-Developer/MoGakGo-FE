73ba0c5e412705e7eb4211970a9a1d0b4022d9ff
mibu119 <horang296@gmail.com>
2024-03-12 22:12:46 +0900

    style: checkbox 디폴트 배경색 neoGreen으로 수정

--- app/_common/shadcn/ui/checkbox.tsx
+++ app/_common/shadcn/ui/checkbox.tsx
@@ -8,7 +8,7 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { cn } from "@/app/_common/shadcn/utils";
 
 const CheckboxVariant = cva(
-  "peer h-4 w-4 shrink-0 rounded-sm border border-black shadow-neo-thin ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-slate-900 data-[state=checked]:text-slate-50 dark:border-slate-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900",
+  "peer h-4 w-4 shrink-0 rounded-sm border border-black shadow-neo-thin ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-neoGreen data-[state=checked]:text-slate-50 dark:border-slate-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=checked]:bg-slate-50 dark:data-[state=checked]:text-slate-900",
   {
     variants: {
       checkedBorder: {
