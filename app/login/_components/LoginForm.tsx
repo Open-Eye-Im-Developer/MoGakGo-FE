21897f907a7126e5e39b63502a64467b7d789bba
mibu119 <horang296@gmail.com>
2024-03-14 21:21:22 +0900

    style: logo 픽셀로보로보체 설정

--- app/login/_components/LoginForm.tsx
+++ app/login/_components/LoginForm.tsx
@@ -5,12 +5,10 @@ import Link from "next/link";
 import StartWithGithubButton from "./StartWithGithubButton";
 
 function LoginForm() {
-  // TODO: 로그인 페이지를 url 변경으로 직접 들어가지 못하도록 next/redirects 설정
-
   return (
     <form className="mt-16 flex w-full flex-col place-items-center space-y-16">
       <div>
-        <section className="grid h-56 w-[297px] place-content-center rounded-lg border-8 border-black bg-gradient-to-b from-[#9FB386] to-[#6F9440] text-center text-5xl">
+        <section className="font-logo grid h-56 w-[297px] place-content-center rounded-lg border-8 border-black bg-gradient-to-b from-[#9FB386] to-[#6F9440] text-center text-5xl">
           <h1>MOGAK</h1>
           <h1>GO</h1>
         </section>
