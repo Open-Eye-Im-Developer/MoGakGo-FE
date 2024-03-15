8233fc2d5bdb36419223f5c9eb720842eceb3b60
lunarmoon7 <zentechie7@gmail.com>
2024-03-15 12:18:47 +0900

    feat: error 발생 시 처리 로직 추가

--- app/project/_components/ProjectManageSection.tsx
+++ app/project/_components/ProjectManageSection.tsx
@@ -3,14 +3,26 @@
 import React, { useEffect } from "react";
 
 import { useMatchingStore } from "@/app/_common/store/useMatchingStore";
+import { useToast } from "@/app/_common/shadcn/ui/use-toast";
 
 import useGetCurrentProjectQuery from "../_hooks/useGetCurrentProjectQuery";
 import ProjectCreateDialog from "./ProjectCreateDialog";
 import ProjectCardContainer from "./ProjectCardContainer";
 
 function ProjectManageSection() {
-  const { project, matchingId } = useGetCurrentProjectQuery();
+  const { project, matchingId, isError, error } = useGetCurrentProjectQuery();
   const { setMatchingId } = useMatchingStore();
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (isError) {
+      toast({
+        title: "프로젝트 정보를 가져오는 중 오류가 발생했습니다.",
+        description: "잠시 후 다시 시도해주세요.",
+        variant: "destructive",
+      });
+    }
+  }, [error, isError]);
 
   useEffect(() => {
     setMatchingId(matchingId);
@@ -21,8 +33,8 @@ function ProjectManageSection() {
       {!project && (
         <div className="flex h-[300px] flex-col items-center justify-between rounded-xl border-4 border-dotted p-10 md:h-[550px]">
           <header className="space-y-3 text-center text-[#7b7b7b]">
-            <h1 className="text-2xl font-bold">생성한 프로젝트가 없어요!</h1>
-            <h2>프로젝트를 새로 생성해주세요.</h2>
+            <h1 className="text-lg font-bold">생성한 프로젝트가 없어요!</h1>
+            <h2 className="text-sm">프로젝트를 새로 생성해주세요.</h2>
           </header>
           <ProjectCreateDialog />
         </div>
--- app/project/_hooks/useGetCurrentProjectQuery.ts
+++ app/project/_hooks/useGetCurrentProjectQuery.ts
@@ -8,12 +8,12 @@ function useGetCurrentProjectQuery() {
   const { user } = useAuthStore();
 
   const getCurrentProject = async () => {
-    const { data } = await instance.get(`/projects/${user!.id}/info`);
+    const { data } = await instance.get(`/projects/12/info`);
 
     return data;
   };
 
-  const { data, isLoading } = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["current-project"],
     queryFn: getCurrentProject,
     staleTime: Infinity,
@@ -21,12 +21,13 @@ function useGetCurrentProjectQuery() {
     throwOnError: false,
   });
 
-  if (isLoading || !data) return { project: null, matchingId: null };
+  if (isLoading || !data)
+    return { project: null, matchingId: null, isError, error };
 
   const project = data.response[0];
   const matchingId = data.matchingId;
 
-  return { project, matchingId };
+  return { project, matchingId, isError, error };
 }
 
 export default useGetCurrentProjectQuery;
