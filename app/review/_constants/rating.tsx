c327f9a852ab67bc3e09f06a396ea1a4eafc9ed0
mibu119 <horang296@gmail.com>
2024-03-15 12:33:08 +0900

    style: 리뷰 아이콘 변경

--- app/review/_constants/rating.tsx
+++ app/review/_constants/rating.tsx
@@ -1,27 +1,27 @@
 export const Ratings = [
   {
     rate: 1,
-    type: "sad-dizzy",
+    type: "review-terrible",
     content: "최악이에요",
   },
   {
     rate: 2,
-    type: "sad",
+    type: "review-bad",
     content: "별로에요",
   },
   {
     rate: 3,
-    type: "smile",
+    type: "review-ok",
     content: "보통이에요",
   },
   {
     rate: 4,
-    type: "happy",
+    type: "review-good",
     content: "좋아요",
   },
   {
     rate: 5,
-    type: "xd",
+    type: "review-awesome",
     content: "최고에요",
   },
 ] as const;
