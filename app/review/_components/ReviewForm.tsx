f311241def93878847619e7f03198348f06db0c3
mibu119 <horang296@gmail.com>
2024-03-17 17:36:53 +0900

    style: 리뷰 페이지 스타일 수정

--- app/review/_components/ReviewForm.tsx
+++ app/review/_components/ReviewForm.tsx
@@ -8,6 +8,7 @@ import { useAuthStore } from "@/app/_common/store/useAuthStore";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
 } from "@/app/_common/shadcn/ui/form";
@@ -59,50 +60,54 @@ function ReviewForm() {
   return (
     <Form {...form}>
       <form
-        className="z-10 flex flex-col place-content-center space-y-8 rounded-md border-2 border-black bg-white p-6 shadow-neo"
+        className="z-10 flex flex-col place-content-center rounded-md border-2 border-black bg-white p-6 shadow-neo"
         onSubmit={form.handleSubmit(onSubmitReview)}
       >
-        <header className="text-center">
-          <h2 className="text-xl text-black">리뷰 남기기</h2>
-          {/* <FormDescription className="text-gray-500">
-            오늘의 만난 친구에 대해서 리뷰를 남겨주세요.
-          </FormDescription> */}
-        </header>
         <FormField
           name="rating"
           control={form.control}
           render={({ field }) => (
-            <FormItem className="space-y-5">
-              <FormControl className="flex w-full items-center">
-                <ul className="flex w-full justify-center gap-5 py-5">
-                  {Ratings.map(({ type, content }, index) => (
-                    <li
-                      className="flex cursor-pointer flex-col items-center gap-1"
-                      key={index}
-                      id={index.toString()}
-                      onClick={handleClickRating}
-                    >
-                      <RatingItem
-                        type={type}
-                        checked={field.value === index + 1}
-                        onChangeChecked={() =>
-                          form.setValue("rating", index + 1)
-                        }
-                      />
-                      <small className="text-nowrap text-[0.6rem] text-gray-500">
-                        {content}
-                      </small>
-                    </li>
-                  ))}
-                </ul>
-              </FormControl>
+            <FormItem className="space-y-14">
+              <div className="space-y-2">
+                <header className="space-y-1 text-left">
+                  <h2 className="text-xl text-black">리뷰 남기기</h2>
+                  <FormDescription className="text-xs text-gray-500">
+                    오늘의 만난 친구에 대해서 리뷰를 남겨주세요.
+                  </FormDescription>
+                </header>
+                <section className="sapce-y-1">
+                  <FormControl className="flex w-full items-center">
+                    <ul className="flex w-full justify-center gap-5 pt-5">
+                      {Ratings.map(({ type, content }, index) => (
+                        <li
+                          className="flex cursor-pointer flex-col items-center gap-1"
+                          key={index}
+                          id={index.toString()}
+                          onClick={handleClickRating}
+                        >
+                          <RatingItem
+                            type={type}
+                            checked={field.value === index + 1}
+                            onChangeChecked={() =>
+                              form.setValue("rating", index + 1)
+                            }
+                          />
+                          <small className="text-nowrap text-[0.6rem] text-gray-500">
+                            {content}
+                          </small>
+                        </li>
+                      ))}
+                    </ul>
+                  </FormControl>
+                  <small className="my-5 block w-full text-end text-neoYellow">
+                    <p className="text-xs">
+                      여러분의 리뷰가 상대방의 잔디력에 영향을 줍니다.
+                    </p>
+                    <p className="text-xs">신중하게 결정해주세요!</p>
+                  </small>
+                </section>
+              </div>
               <footer className="flex flex-col items-end justify-between pt-10">
-                <small className="my-5 flex w-full flex-col text-end text-gray-500">
-                  <p className="text-xs">
-                    여러분의 리뷰가 상대방의 잔디력에 영향을 줍니다.
-                  </p>
-                  <p className="text-xs">신중하게 결정해주세요!</p>
-                </small>
                 <section className="flex w-full items-center justify-between">
                   <small className="cursor-pointer text-gray-500 hover:text-neoRed">
                     <Link href={"/"}>리뷰 안하기</Link>
