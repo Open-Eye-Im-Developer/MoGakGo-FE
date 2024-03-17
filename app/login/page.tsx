bf6153845a5d2ca2c7556330678f1dcda5ff856a
mibu <114740795+mibu119@users.noreply.github.com>
2024-03-17 15:24:57 +0900

    [style] 로그인 페이지 새로운 디자인으로 변경 (#181)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 import LoginForm from "./_components/LoginForm";
 
 function LoginPage() {
   return (
-    <main className="flex h-full min-h-[740px] flex-col items-center justify-center">
+    <main className="relatie flex h-screen w-full flex-col items-center bg-[#E8E8E8]">
+      <Image
+        className="absolute bottom-16 right-0"
+        src={"/images/login_bg_imgs.png"}
+        alt="게임 버튼"
+        width={100}
+        height={100}
+      />
       <LoginForm />
     </main>
   );
