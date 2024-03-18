fce5a66b25a1b85692ac542732f314b4eb46e370
mibu <114740795+mibu119@users.noreply.github.com>
2024-03-18 15:24:29 +0900

    [style] 업적 페이지 새로운 디자인으로 변경 (#203)

--- app/achievements/page.tsx
+++ app/achievements/page.tsx
@@ -1,10 +1,16 @@
+import Icon from "../_common/components/Icon";
 import AchievementList from "./_components/AchievementList";
 export const dynamic = "force-dynamic";
 
 function AchievementsPage() {
   return (
     <main className="p-4">
-      <h2 className="py-3 text-center text-xl font-semibold">활동 업적</h2>
+      {/* TODO: 공통 헤더 컴포넌트로 변경 후 router 추가 */}
+      <h2 className="relative py-3 text-center text-xl">
+        <Icon id="chevron-left" size={24} className="absolute" />
+        활동 업적
+      </h2>
+
       <AchievementList />
     </main>
   );
