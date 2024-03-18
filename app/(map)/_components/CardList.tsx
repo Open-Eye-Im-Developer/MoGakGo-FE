3ffd409c3a2054fc8e34ad156d50e042e869f324
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-03-18 11:59:38 +0900

    style: 프로젝트 카드와 프로필 카드 위치 불균형으로 패딩 설정

--- app/(map)/_components/CardList.tsx
+++ app/(map)/_components/CardList.tsx
@@ -27,14 +27,14 @@ function CardList({ cardList, onClick }: Props) {
     <>
       <CarouselContent>
         {projectList.map((project: Project) => (
-          <CarouselItem key={project.projectId}>
+          <CarouselItem className="pb-11" key={project.projectId}>
             <div className="flex items-center justify-center">
               <ProjectCardContainer project={project} />
             </div>
           </CarouselItem>
         ))}
         {profileList.map((profile: Profile) => (
-          <CarouselItem key={profile.response.id}>
+          <CarouselItem className="pt-11" key={profile.response.id}>
             <div className="flex items-center justify-center">
               <ProfileCardItem profile={profile} />
             </div>
