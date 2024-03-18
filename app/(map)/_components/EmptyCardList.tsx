53a8eeb61b3b699d321d3346618f31648a635aa8
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-03-18 11:47:22 +0900

    style: 조회할 수 있는 카드가 없는 경우 카드 컴포넌트 스타일 변경

--- app/(map)/_components/EmptyCardList.tsx
+++ app/(map)/_components/EmptyCardList.tsx
@@ -1,5 +1,4 @@
-import { IconRotateClockwise2 } from "@tabler/icons-react";
-
+import { Card, CardContent } from "@/app/_common/shadcn/ui/card";
 import { Button } from "@/app/_common/shadcn/ui/button";
 
 interface Props {
@@ -8,15 +7,14 @@ interface Props {
 
 function EmptyCardList({ onClick }: Props) {
   return (
-    <div className="card h-[550px] w-[330px] bg-white/40 opacity-[0.99] backdrop-blur-md sm:w-[450px]">
-      <div className="flex h-full w-full flex-col items-center justify-center text-center">
-        <p>조회할 수 있는 카드 리스트가 없습니다.</p>
-        <Button className="mt-2 bg-primary" onClick={onClick}>
-          <IconRotateClockwise2 className="mr-1 h-5 w-5" />
+    <Card className="h-[180px] w-[330px] sm:w-[450px]">
+      <CardContent className="relative flex h-full w-full flex-col items-center justify-center p-0 text-center">
+        <p className="pb-2">조회할 수 있는 카드 리스트가 없습니다.</p>
+        <Button className="absolute bottom-5 bg-neoYellow" onClick={onClick}>
           돌아가기
         </Button>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   );
 }
 
