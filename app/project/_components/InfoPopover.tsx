8fb6080abe8638376107bb97536c3263fe314dfd
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-03-18 10:41:21 +0900

    [style] 카드 컴포넌트 스타일 변경 (#201)

---
+++ app/project/_components/InfoPopover.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/app/_common/shadcn/ui/popover";
+import { Button } from "@/app/_common/shadcn/ui/button";
+import { Badge } from "@/app/_common/shadcn/ui/badge";
+
+import Icon from "@/app/_common/components/Icon";
+
+import { WantedJobs } from "@/app/_common/types/user";
+
+type InfoType = "LANG" | "INTEREST";
+
+interface InfoPopoverProps {
+  type: InfoType;
+  infoList: string[] | WantedJobs[];
+}
+
+function InfoPopover(props: InfoPopoverProps) {
+  const { type, infoList } = props;
+  const title = type === "LANG" ? "개발 언어" : "관심 직무";
+  const iconId = type === "LANG" ? "info-tech" : "info-job";
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="ghost" className="flex items-center gap-10">
+          <span className="text-lg">{title}</span>
+          <Icon id="chevron-right" className="h-6 w-6" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="max-w-[200px] space-y-2">
+        <header className="flex items-center gap-3 text-lg">
+          <p>{title}</p>
+          <Icon id={iconId} className="h-6 w-6" />
+        </header>
+        <main className="flex flex-wrap gap-1">
+          {infoList.map(info => (
+            <Badge key={info} variant="outline">
+              {info}
+            </Badge>
+          ))}
+        </main>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+export default InfoPopover;
