701dfb3bd3276c48ec01d4624353a38189b6df98
lunarmoon7 <zentechie7@gmail.com>
2024-03-19 18:54:34 +0900

    style: 프로필 카드 스타일 수정

--- app/_common/components/ProfileCard.tsx
+++ app/_common/components/ProfileCard.tsx
@@ -31,7 +31,6 @@ function ProfileCard(props: ProfileCardProps) {
       githubUrl,
       bio,
       jandiRate,
-      achievementTitle,
       developLanguages,
       wantedJobs,
     },
@@ -42,7 +41,7 @@ function ProfileCard(props: ProfileCardProps) {
   return (
     <Card
       className={cn(
-        "h-full w-full border-none shadow-none",
+        "card relative flex h-full w-full flex-col border-none shadow-none",
         isBehind
           ? "absolute inset-0 left-0 top-0 overflow-hidden [backface-visibility:hidden] [transform:rotateY(180deg)]"
           : "",
@@ -51,16 +50,16 @@ function ProfileCard(props: ProfileCardProps) {
       <CardHeader className="px-4 py-3">
         <CardDescription className="flex items-center justify-between text-lg font-bold text-black">
           {isBehind && onRotate && <ButtonRotate onRotate={onRotate} />}
-          <Link href={githubUrl} target="_blank" className="text-[#a2a2a2]">
+          <Link href={githubUrl} target="_blank">
             @{githubId}
           </Link>
         </CardDescription>
       </CardHeader>
-      <CardContent className="flex flex-col items-center gap-8 px-3 py-2">
+      <CardContent className="flex grow flex-col items-center gap-8 px-3 py-2">
         <div className="flex w-full flex-col gap-3">
-          <div className="flex w-full justify-between ">
+          <div className="flex w-full justify-between sm:justify-center">
             <div className="relative ml-2 rounded-full">
-              <Avatar className="h-52 w-52">
+              <Avatar className="h-52 w-52 bg-[#ffffff]">
                 <AvatarImage src={avatarUrl} alt="profile" />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
@@ -71,21 +70,28 @@ function ProfileCard(props: ProfileCardProps) {
               </div>
               <div className="flex flex-col gap-2">
                 <YProgress value={jandiRate} background="green" />
-                <span className="text-xs text-[#868686]">
-                  {jandiRate !== undefined ? jandiRate * 100 : ""}%
+                <span className="text-xs">
+                  {jandiRate && jandiRate < 0 ? "NaN" : jandiRate}%
                 </span>
               </div>
             </div>
           </div>
           <div className="flex flex-col items-center gap-4">
             <div className="flex flex-col items-center gap-1 p-1">
-              <h1 className="text-2xl font-bold">{username}</h1>
-              <h3 className="text-xs text-[#F76A6A]">{achievementTitle}</h3>
+              <h1 className="relative inline-block text-2xl font-bold before:absolute before:-inset-2 before:block before:-skew-y-3 before:border-2 before:border-black before:bg-gradient-to-r before:from-[#FF915E] before:to-[#552AFF]">
+                <span className="relative text-black">{username}</span>
+              </h1>
+              <h3 className="mt-3 text-xs font-bold text-[#F76A6A]">
+                {"이세계 개발자"}
+              </h3>
             </div>
-            <p className="w-60 overflow-hidden text-center text-sm">{bio}</p>
+            <p className="line-clamp-3 w-40 overflow-hidden text-center text-sm">
+              {bio ??
+                "엄마 엄마 엄마 밖에 눈이와요. 하얀 눈이 와요 이야 이쁘다아아아아ㅏ아앙"}
+            </p>
           </div>
-          <div className="flex flex-col items-center">
-            <InfoPopover type="INTEREST" infoList={wantedJobs!} />
+          <div className="flex items-center justify-center gap-2">
+            <InfoPopover type="INTEREST" infoList={wantedJobs} />
             <InfoPopover type="LANG" infoList={developLanguages} />
           </div>
         </div>
