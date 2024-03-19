84ffc4bf795abd361770db106ef1729820603241
bel1c10ud <mail@bellclou.de>
2024-03-19 14:31:39 +0900

    fix: 변경된 props 이름 반영

--- app/my-page/delete/page.tsx
+++ app/my-page/delete/page.tsx
@@ -7,7 +7,7 @@ import DeleteUserButton from "../_components/DeleteUserButton";
 function DeletePage() {
   return (
     <>
-      <StackNavigator content={"회원 탈퇴"} />
+      <StackNavigator element={"회원 탈퇴"} />
       <main className="container flex min-h-screen max-w-2xl flex-col gap-8 pb-8">
         <div className="flex flex-col gap-4">
           {/* TODO: 추가 고지 사항 작성 */}
