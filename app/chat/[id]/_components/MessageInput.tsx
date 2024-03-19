7027e84eb3f453930a913e3a755db8ff148d131c
yougyung <youkyeongs@naver.com>
2024-03-19 10:23:46 +0900

    style: 채팅 상세 페이지 리디자인 반영

--- app/chat/[id]/_components/MessageInput.tsx
+++ app/chat/[id]/_components/MessageInput.tsx
@@ -5,7 +5,7 @@ import { Client } from "@stomp/stompjs";
 
 import { useAuthStore } from "@/app/_common/store/useAuthStore";
 import { cn } from "@/app/_common/shadcn/utils";
-import { buttonVariants } from "@/app/_common/shadcn/ui/button";
+import { Button } from "@/app/_common/shadcn/ui/button";
 
 import { ChatType } from "../../_types/chat";
 import useGetChats from "../../_api/useGetChats";
@@ -72,25 +72,26 @@ function MessageInput({
   };
 
   return (
-    <div className="fixed bottom-0 h-32 w-full bg-slate-100">
+    <div className="fixed bottom-0 h-32 w-full">
       <textarea
-        className="h-20 w-full overflow-scroll bg-slate-100 p-3 outline-none"
+        className="h-20 w-full overflow-scroll border-t-[1px] border-black p-3 outline-none"
         onKeyDown={handleKeydownTextArea}
         rows={4}
         value={message}
         onChange={e => handleChangeMessage(e.target.value)}
       />
-      <button
+
+      <Button
+        type="button"
         onClick={handleSubmit}
         contentEditable={false}
         className={cn(
-          buttonVariants({ variant: "secondary" }),
-          "fixed bottom-0 right-0 m-1 h-8 bg-slate-200 hover:bg-slate-300",
+          "fixed bottom-0 right-0 m-2 bg-neoGreen",
           isActiveRoom === "CLOSE" && "hidden",
         )}
       >
         전송
-      </button>
+      </Button>
     </div>
   );
 }
