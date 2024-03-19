2a63a224679cf8791399198a5d7fdb9964e1fd8e
mibu119 <horang296@gmail.com>
2024-03-20 00:35:23 +0900

    test: sortingLanguages test에 imageUrl 추가

--- app/signup/_test/sortingLanguages.test.ts
+++ app/signup/_test/sortingLanguages.test.ts
@@ -5,14 +5,17 @@ describe("sortingLanguages", () => {
   it("should sort the data by lines in descending order and calculate the percentage", () => {
     const mockData: Language[] = [
       {
+        imageUrl: "https://www.google.com",
         language: "JavaScript",
         byteSize: 3000,
       },
       {
+        imageUrl: "https://www.google.com",
         language: "TypeScript",
         byteSize: 5000,
       },
       {
+        imageUrl: "https://www.google.com",
         language: "CSS",
         byteSize: 2000,
       },
@@ -22,16 +25,19 @@ describe("sortingLanguages", () => {
 
     expect(result).toEqual([
       {
+        imageUrl: "https://www.google.com",
         language: "TypeScript",
         byteSize: 5000,
         percentage: 50.0,
       },
       {
+        imageUrl: "https://www.google.com",
         language: "JavaScript",
         byteSize: 3000,
         percentage: 30.0,
       },
       {
+        imageUrl: "https://www.google.com",
         language: "CSS",
         byteSize: 2000,
         percentage: 20.0,
