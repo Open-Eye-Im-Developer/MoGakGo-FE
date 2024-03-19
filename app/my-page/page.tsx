e6d770a4519c0ecc1c8d1096a6768baa8786f182
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-03-19 13:45:25 +0900

    style: 하단 네비게이션 추가로 카드를 가리는 현상이 있어 하단 패딩 변경

--- app/my-page/page.tsx
+++ app/my-page/page.tsx
@@ -30,7 +30,7 @@ function MyPage() {
   }
 
   return (
-    <main className="container flex min-h-screen max-w-2xl flex-col gap-8 bg-gray-50 pb-8 dark:bg-gray-950">
+    <main className="container flex min-h-screen max-w-2xl flex-col gap-8 bg-gray-50 pb-24 dark:bg-gray-950">
       <StackNavigator element={"마이페이지"} />
       <Profile data={userData} />
       <LikeCounter
