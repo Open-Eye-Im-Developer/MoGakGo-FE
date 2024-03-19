c31a7944ebb962b30afa68d7221718f9024a1454
mibu119 <horang296@gmail.com>
2024-03-19 09:59:45 +0900

    chore: 빌드 에러 해결, 로그인 페이지에 suspense 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,22 @@
+import { Suspense } from "react";
 import Image from "next/image";
 
 import LoginForm from "./_components/LoginForm";
 
 function LoginPage() {
   return (
-    <main className="relatie flex h-screen w-full flex-col items-center bg-[#E8E8E8]">
-      <Image
-        className="absolute bottom-16 right-0"
-        src={"/images/login_bg_imgs.png"}
-        alt="게임 버튼"
-        width={100}
-        height={100}
-      />
-      <LoginForm />
-    </main>
+    <Suspense>
+      <main className="relatie flex h-screen w-full flex-col items-center bg-[#E8E8E8]">
+        <Image
+          className="absolute bottom-16 right-0"
+          src={"/images/login_bg_imgs.png"}
+          alt="게임 버튼"
+          width={100}
+          height={100}
+        />
+        <LoginForm />
+      </main>
+    </Suspense>
   );
 }
 
