5d7efd4d991b859fb441556283ff96b5c5dded13
lunarmoon7 <zentechie7@gmail.com>
2024-03-20 18:47:44 +0900

    style: 스타일 수정

--- app/project/_components/ProjectManageSection.tsx
+++ app/project/_components/ProjectManageSection.tsx
@@ -1,22 +1,14 @@
 "use client";
 
 import React, { useEffect } from "react";
-import Link from "next/link";
 
-import { useQueryUserData } from "@/app/my-page/_hooks/useQueryUserData";
-import { useQueryCurrentMatchingProject } from "@/app/my-page/_hooks/useQueryCurrentMatchingProject";
 import { useToast } from "@/app/_common/shadcn/ui/use-toast";
-import { Button } from "@/app/_common/shadcn/ui/button";
 
-import LoadingSpinner from "@/app/_common/components/LoadingSpinner";
-
+import useGetCurrentProjectQuery from "../_hooks/useGetCurrentProjectQuery";
 import ProjectCreateDialog from "./ProjectCreateDialog";
 
 function ProjectManageSection() {
-  const { data: userData } = useQueryUserData();
-  const { project, isLoading, isError } = useQueryCurrentMatchingProject(
-    userData?.id,
-  );
+  const { project, matchingId, isError } = useGetCurrentProjectQuery();
   const { toast } = useToast();
 
   useEffect(() => {
@@ -29,39 +21,40 @@ function ProjectManageSection() {
     }
   }, [isError]);
 
-  if (isLoading) {
-    return <LoadingSpinner />;
-  }
-
-  console.log(project);
   return (
     <main className="relative flex h-full w-full flex-col items-center justify-center">
       <div className="map-background" />
       <div className="map-background" />
-      {!project && (
-        <div className="z-50 mx-10 flex h-[300px] flex-col items-center justify-between rounded-xl border-4 border-dotted border-black p-10 md:h-[550px]">
-          <header className="space-y-3 text-center">
-            {!project ? (
-              <>
-                <h1 className="text-lg font-bold">
-                  생성, 매칭된 프로젝트가 없어요!
-                </h1>
-                <h2 className="text-sm">
-                  프로젝트를 새로 생성하거나 매칭을 기다려주세요!
-                </h2>
-              </>
-            ) : (
-              <>
-                <h1 className="text-lg font-bold">프로젝트가 매칭되었어요!</h1>
-                <Button>
-                  <Link href="/my-page" />
-                </Button>
-              </>
-            )}
-          </header>
-          <ProjectCreateDialog />
-        </div>
-      )}
+      <div className="z-50 mx-10 flex h-[300px] flex-col items-center justify-center rounded-xl border-4 border-dotted border-black p-10 md:h-[550px]">
+        <header className="space-y-3 text-center">
+          {!project && (
+            <>
+              <h1 className="text-base font-bold">생성된 프로젝트가 없어요!</h1>
+              <h2 className="text-sm">
+                프로젝트를 새로 생성하거나 매칭을 기다려주세요!
+              </h2>
+              <ProjectCreateDialog />
+            </>
+          )}
+          {project && matchingId === null && (
+            <>
+              <h1 className="text-base font-bold">매칭된 프로젝트가 없어요!</h1>
+              <h2 className="text-sm">
+                프로젝트 매칭을 기다려주세요! 프로젝트 관리는 마이 페이지에서 할
+                수 있어요!
+              </h2>
+            </>
+          )}
+          {project && matchingId && (
+            <>
+              <h1 className="text-base font-bold">프로젝트가 매칭되었어요!</h1>
+              <h2 className="text-sm">
+                채팅방으로 이동해서 자세한 얘기를 나눠봐요!
+              </h2>
+            </>
+          )}
+        </header>
+      </div>
     </main>
   );
 }
