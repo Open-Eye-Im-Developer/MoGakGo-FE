12513b4c048205563ab840160a5701e89baddead
mibu119 <horang296@gmail.com>
2024-03-20 14:07:03 +0900

    cleanup: instance reissue 버그 주석 처리

--- app/_common/api/instance.ts
+++ app/_common/api/instance.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-import { getCookie } from "../utils/cookie";
-import { reIssueAccessToken } from "./auth";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const SERVER_VERSION = "/api/v1";
 
@@ -28,24 +26,24 @@ instance.interceptors.response.use(
     return response;
   },
   async error => {
-    const { status, config } = error.response;
-    // TODO: 백엔드에서 가져온 cookie를 브라우저에서 get 하는지 확인하기
-    const refreshToken = await getCookie("refreshToken", "");
+    // TODO: refreshtoken, cookie 버그 해결될 때까지 주석 유지
+    // const { status, config } = error.response;
 
-    // TODO: accessToken을 쿠키에 저장하여 1시간 이후가 되면 자동 갱신하도록 수정?
-    if (status === 404) {
-      const newAccessToken = await reIssueAccessToken(refreshToken);
+    // const refreshToken = await getCookie("refreshToken", "");
 
-      if (newAccessToken) {
-        localStorage.setItem("accessToken", newAccessToken);
+    // if (status === 404) {
+    //   const newAccessToken = await reIssueAccessToken(refreshToken);
 
-        config.headers.Authorization = `Bearer ${newAccessToken}`;
+    //   if (newAccessToken) {
+    //     localStorage.setItem("accessToken", newAccessToken);
 
-        return instance(config);
-      }
+    //     config.headers.Authorization = `Bearer ${newAccessToken}`;
 
-      return Promise.reject(error);
-    }
+    //     return instance(config);
+    //   }
+
+    //   return Promise.reject(error);
+    // }
 
     return Promise.reject(error);
   },
