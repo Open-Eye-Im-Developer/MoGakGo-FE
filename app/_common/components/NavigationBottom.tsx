7a5c0c4d87592be43a70d965dc17174b2ec648b8
Baeg <73841260+bgyoons@users.noreply.github.com>
2024-03-20 10:56:37 +0900

    refactor: 코드 위치 변경

--- app/_common/components/NavigationBottom.tsx
+++ app/_common/components/NavigationBottom.tsx
@@ -33,13 +33,6 @@ function NavigationBottom() {
       defaultValue={pathname}
       className="fixed bottom-8 left-2/4 z-100 -translate-x-2/4 shadow-neo-b"
     >
-      <div
-        className={cn(
-          "fixed bottom-0 left-20 right-0 top-0 min-h-10",
-          getUser() && "hidden",
-        )}
-        onClick={handleNoAuthClick}
-      />
       <MenubarMenu value="/">
         <MenubarTrigger
           value="/"
@@ -76,6 +69,13 @@ function NavigationBottom() {
           모각고
         </MenubarTrigger>
       </MenubarMenu>
+      <div
+        className={cn(
+          "fixed bottom-0 left-20 right-0 top-0 min-h-10",
+          getUser() && "hidden",
+        )}
+        onClick={handleNoAuthClick}
+      />
     </Menubar>
   );
 }
