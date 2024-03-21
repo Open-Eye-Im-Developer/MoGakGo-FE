cf71ebbfa812633818b8be24b28387f2af8f9718
lunarmoon7 <zentechie7@gmail.com>
2024-03-21 13:39:30 +0900

    refactor: 업적 API 변경

--- app/_common/components/WebSocketProvider.tsx
+++ app/_common/components/WebSocketProvider.tsx
@@ -7,7 +7,8 @@ import { useQueryUserData } from "@/app/my-page/_hooks/useQueryUserData";
 import { cn } from "../shadcn/utils";
 import { useToast } from "../shadcn/ui/use-toast";
 import { Progress } from "../shadcn/ui/progress";
-import { getAchievements } from "../api/achievements";
+import { Avatar, AvatarFallback, AvatarImage } from "../shadcn/ui/avatar";
+import { getAchievementDetail } from "../api/achievements";
 
 interface WebSocketMessage {
   userId: number;
@@ -79,29 +80,35 @@ export const WebSocketProvider = ({
 
   useEffect(() => {
     async function getAchievementTitle(message: WebSocketMessage) {
-      const { userId, achievementId, completed, progressCount } = message;
-      const data = await getAchievements(userId);
+      const { achievementId, completed, progressCount } = message;
+      const { title, imgUrl, requirementValue } =
+        await getAchievementDetail(achievementId);
 
-      const currentAchievement = data.find(
-        achievement => achievement.achievementId === achievementId,
-      );
-
-      const achievementTitle = currentAchievement
-        ? currentAchievement.title
-        : "null";
+      const achievementTitle = title ?? "null";
       const toastTitle = completed ? "업적 획득! 🎉" : "업적 진행률 갱신!";
-      const toastDescription = completed
-        ? `<${achievementTitle} /> 업적을 획득했어요!`
-        : `${achievementTitle} 획득까지 앞으로 ${progressCount}회!`;
-
+      const toastDescription = `<${achievementTitle} />`;
+      const percentage =
+        progressCount === 0
+          ? 0
+          : Number((progressCount / requirementValue).toFixed(2)) * 100;
       toast({
         className: cn("bottom-4 right-0 fixed max-w-[350px] mr-3 z-[100]"),
         title: toastTitle,
         description: (
-          <main className="w-full space-y-1 text-xs">
-            <h3>{toastDescription}</h3>
+          <main className="flex w-full flex-col gap-3 space-y-1 text-xs">
+            <aside className="flex items-center gap-3">
+              <Avatar className="h-12 w-12 shadow-neo-thin">
+                <AvatarImage src={imgUrl} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <h3 className="text-base font-bold">
+                <span>{toastDescription}</span>
+                <span> 획득까지 앞으로 </span>
+                <span className="text-neoRed">{progressCount}회!</span>
+              </h3>
+            </aside>
             <Progress
-              value={completed ? 100 : progressCount}
+              value={completed ? 100 : percentage}
               background="blue"
               className="w-full grow"
             />
