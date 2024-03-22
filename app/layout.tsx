b795ea6269cffdde136256b20a6b9975882c6bde
Coggie (HwisikMoon) <zentechie7@gmail.com>
2024-03-22 20:56:48 +0900

    [hotfix] input 클릭 시 축소가 안풀리는 버그 수정 (#341)

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
         content="width=device-width, initial-scale=1, maximum-scale=1.0, 
     user-scalable=0"
       />
-      <body className="touch-pan-x touch-pan-y">
+      <body>
         <ClientProvider>
           <WebSocketProvider>
             <ThemeProvider
